c9143a4a7612a808ebc6ae2d3acaab38b7501e14
DAVI GABRIEL DINIZ CEREJA <davi_cereja@estudante.sesisenai.org.br>
2026-09-16 22:26:42 -0300

    nova pasta de exemplos

---
+++ aula_3/exemplo00.js
@@ -0,0 +1,8 @@
+function somaNumeros(numero1, numero2) {
+    let resultado = numero1 + numero2
+    
+    return resultado;
+  }
+
+  let total = somaNumeros(2,3);
+console.log(`O total da soma é: ${total}`);
---
+++ aula_3/exemplo02.js
@@ -0,0 +1,4 @@
+const multiplicar = (a, b) => {
+    return a * b;
+  };
+    console.log(`o total é: ${multiplicar(9, 3)}`)
